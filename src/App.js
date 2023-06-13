dee0b3ad4dac9849d65395c16db48d9ae0f6f1c7
Nahuel Otamendi <nahuotamendi@gmail.com>
2023-06-13 18:55:36 -0300

    Clase04 finalizada, mandamos el usuario para el componente 404 utilizando la función useNavigate(), mejoramos la experiencia de buscar un artículo creando un filtro por categorías.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from "./pages/Home.jsx";
 import Page404 from "./pages/Page404";
 import Sobre from "./pages/Sobre.jsx";
 import Header from "./componentes/Header";
+import Post from "./pages/Posts";
+
 //Se importa el Router, las rutas y la ruta
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
@@ -14,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/sobre" element={<Sobre />} />
+          <Route path="/posts/:id" element={<Post />} />
           <Route path="*" element={<Page404 />} />
         </Routes>
       </Router>
